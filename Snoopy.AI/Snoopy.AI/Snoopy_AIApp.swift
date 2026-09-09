885b83eb6d4432f5a627a84aaad49c86f42a79af
Lee Roland <glroland@hotmail.com>
2026-09-08 20:37:06 -0400

    Initial project template as generated by xcode

---
+++ Snoopy.AI/Snoopy.AI/Snoopy_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snoopy_AIApp.swift
+//  Snoopy.AI
+//
+//  Created by Lee Roland on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Snoopy_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
